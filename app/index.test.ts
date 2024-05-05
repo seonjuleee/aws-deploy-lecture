ec1f2cf90cc43bffb7191ea08d7224c3f299fc74
seonjuleee <nlsj0820@naver.com>
2024-05-05 21:47:44 +0900

    Supertest로 API 엔드포인트 테스트하기

--- app/index.test.ts
+++ app/index.test.ts
@@ -1,5 +1,29 @@
-describe("jest", () => {
-    it("should work", () => {
-        expect(200).toBe(200);
+import request from "supertest";
+import { App } from "supertest/types";
+import { createApp } from ".";
+
+let app: App;
+
+beforeAll(async () => {
+    app = await createApp();
+});
+
+describe("POST /messages", () => {
+    it("responds with a success message", async () => {
+        const response = await request(app)
+            .post("/messages")
+            .send({ message: "testing with redis" });
+
+        expect(response.statusCode).toBe(200);
+        expect(response.text).toBe("Message added to list.");
+    });
+});
+
+describe("GET /messages", () => {
+    it("responds with all message", async () => {
+        const response = await request(app).get("/messages");
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toEqual([]);
     });
 });
