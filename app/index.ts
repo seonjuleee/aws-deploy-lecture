847d57cf1ce269319a6171d34c06443f53747f0c
seonjuleee <nlsj0820@naver.com>
2024-05-05 21:23:56 +0900

    Redis 활용한 간단한 API 만들기

--- app/index.ts
+++ app/index.ts
@@ -1,15 +1,37 @@
 import express, { response } from "express";
-
-const app = express();
-
-app.use(express.json());
-
-app.get("/", (request, response) => {
-    response.status(200).send("hello from express");
-});
+import * as redis from "redis";
 
 const PORT = 4000;
+const LIST_KEY = "messages";
 
-app.listen(PORT, () => {
-    console.log(`App listening at port ${PORT}`);
+const createApp = async () => {
+    const app = express();
+
+    const client = redis.createClient({ url: "redis://localhost:6379" });
+    await client.connect();
+
+    app.use(express.json());
+
+    app.get("/", (request, response) => {
+        response.status(200).send("hello from express");
+    });
+
+    app.post("/messages", async (request, response) => {
+        const { message } = request.body;
+        await client.lPush(LIST_KEY, message);
+        response.status(200).send("Message added to list.");
+    });
+
+    app.get("/messages", async (request, response) => {
+        const messages = await client.lRange(LIST_KEY, 0, -1);
+        response.status(200).send(messages);
+    });
+
+    return app;
+};
+
+createApp().then((app) => {
+    app.listen(PORT, () => {
+        console.log(`App listening at port ${PORT}`);
+    });
 });
